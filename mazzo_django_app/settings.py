2c3dd5c529be6635a007989cfa92e2a7d54e8fdc
Chris Mazzochi <cmazzochi81@gmail.com>
2020-01-29 09:43:04 -0700

    improving

--- mazzo_django_app/settings.py
+++ mazzo_django_app/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '7mr-4@%*#p(!_$7_m5-ucv$cmf(@qadf42v-zlx2cklsk=it%1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mazzodjangoapp.herokuapp.com']
 
 
 # Application definition
